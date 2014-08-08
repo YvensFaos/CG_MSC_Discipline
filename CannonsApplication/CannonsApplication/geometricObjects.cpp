0b1ab909733d3b78d8bd194c3c4f50006e9ecba5
YvensNotebook <yvensre@gmail.com>
2014-08-08 10:45:09 -0300

    reajuste [3] na organização do projeto

--- CannonsApplication/CannonsApplication/geometricObjects.cpp
+++ CannonsApplication/CannonsApplication/geometricObjects.cpp
@@ -4,25 +4,34 @@
 
 GPlane::GPlane(void)
 {
-
+	initialize(0.0f, 0.0f, nullptr, nullptr);
 }
 
 GPlane::GPlane(float height, float width, EDPoint* position, EDPlane* plane)
 {
-
+	initialize(height, width, position, plane);
 }
 
 GPlane::~GPlane(void)
 {
-
+	if(position)
+		delete position;
+	if(plane)
+		delete plane;
 }
 
 void GPlane::initialize(float height, float width, EDPoint* position, EDPlane* plane)
 {
-
+	this->height = height;
+	this->width = width;
+	this->position = position;
+	this->plane = plane;
 }
 
 void GPlane::draw(void)
-
 {
+	glBegin(GL_QUADS);
+
+
+	glEnd();
 }
