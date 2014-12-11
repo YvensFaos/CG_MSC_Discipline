9683c52ee91aa001bf9388822f447312e5e6535d
YvensDesktop <yvensre@gmail.com>
2014-12-10 23:59:23 -0300

    fazendo o morphing

---
+++ MorphingAlgorithm/MorphingAlgorithm/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace MorphingAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelStart = new System.Windows.Forms.Panel();
+            this.panelFinish = new System.Windows.Forms.Panel();
+            this.labelStart = new System.Windows.Forms.Label();
+            this.labelFinish = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonFinish = new System.Windows.Forms.Button();
+            this.panelStep = new System.Windows.Forms.Panel();
+            this.buttonStep = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelStart
+            // 
+            this.panelStart.Location = new System.Drawing.Point(12, 12);
+            this.panelStart.Name = "panelStart";
+            this.panelStart.Size = new System.Drawing.Size(374, 388);
+            this.panelStart.TabIndex = 0;
+            this.panelStart.Click += new System.EventHandler(this.PanelStart_Click);
+            // 
+            // panelFinish
+            // 
+            this.panelFinish.Location = new System.Drawing.Point(392, 12);
+            this.panelFinish.Name = "panelFinish";
+            this.panelFinish.Size = new System.Drawing.Size(374, 388);
+            this.panelFinish.TabIndex = 1;
+            this.panelFinish.Click += new System.EventHandler(this.PanelFinish_Click);
+            // 
+            // labelStart
+            // 
+            this.labelStart.AutoSize = true;
+            this.labelStart.Location = new System.Drawing.Point(12, 407);
+            this.labelStart.Name = "labelStart";
+            this.labelStart.Size = new System.Drawing.Size(35, 13);
+            this.labelStart.TabIndex = 2;
+            this.labelStart.Text = "label1";
+            // 
+            // labelFinish
+            // 
+            this.labelFinish.AutoSize = true;
+            this.labelFinish.Location = new System.Drawing.Point(389, 407);
+            this.labelFinish.Name = "labelFinish";
+            this.labelFinish.Size = new System.Drawing.Size(35, 13);
+            this.labelFinish.TabIndex = 3;
+            this.labelFinish.Text = "label2";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(311, 407);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Voltar";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonFinish
+            // 
+            this.buttonFinish.Location = new System.Drawing.Point(691, 407);
+            this.buttonFinish.Name = "buttonFinish";
+            this.buttonFinish.Size = new System.Drawing.Size(75, 23);
+            this.buttonFinish.TabIndex = 5;
+            this.buttonFinish.Text = "Voltar";
+            this.buttonFinish.UseVisualStyleBackColor = true;
+            this.buttonFinish.Click += new System.EventHandler(this.buttonFinish_Click);
+            // 
+            // panelStep
+            // 
+            this.panelStep.Location = new System.Drawing.Point(772, 12);
+            this.panelStep.Name = "panelStep";
+            this.panelStep.Size = new System.Drawing.Size(374, 388);
+            this.panelStep.TabIndex = 2;
+            // 
+            // buttonStep
+            // 
+            this.buttonStep.Location = new System.Drawing.Point(773, 406);
+            this.buttonStep.Name = "buttonStep";
+            this.buttonStep.Size = new System.Drawing.Size(75, 23);
+            this.buttonStep.TabIndex = 6;
+            this.buttonStep.Text = "Avançar";
+            this.buttonStep.UseVisualStyleBackColor = true;
+            this.buttonStep.Click += new System.EventHandler(this.buttonStep_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1153, 434);
+            this.Controls.Add(this.buttonStep);
+            this.Controls.Add(this.panelStep);
+            this.Controls.Add(this.buttonFinish);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelFinish);
+            this.Controls.Add(this.labelStart);
+            this.Controls.Add(this.panelFinish);
+            this.Controls.Add(this.panelStart);
+            this.Name = "Form1";
+            this.Text = "Morphing";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelStart;
+        private System.Windows.Forms.Panel panelFinish;
+        private System.Windows.Forms.Label labelStart;
+        private System.Windows.Forms.Label labelFinish;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonFinish;
+        private System.Windows.Forms.Panel panelStep;
+        private System.Windows.Forms.Button buttonStep;
+
+    }
+}
+
---
+++ MorphingAlgorithm/MorphingAlgorithm/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MorphingAlgorithm
+{
+    public partial class Form1 : Form
+    {
+        bool line1;
+        bool line2;
+
+        Point p1;
+        Point p2;
+
+        List<Line> lines1 = new List<Line>();
+        List<Line> lines2 = new List<Line>();
+
+        Graphics gStart;
+        Graphics gFinish;
+        Graphics gStep;
+
+        Image startImg;
+        Image finishImg;
+
+        Bitmap startBmp;
+        Bitmap finishBmp;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            line1 = false;
+            line2 = false;
+
+            this.panelStart.Paint += PanelStart_Paint;
+            this.panelFinish.Paint += PanelFinish_Paint;
+            this.panelStep.Paint += PanelStep_Paint;
+        }
+
+        private void PanelStart_Paint(object sender, PaintEventArgs e)
+        {
+            startImg = Image.FromFile("C:\\Users\\Yvens\\Documents\\GitHub\\DisciplinaCG\\MorphingAlgorithm\\MorphingAlgorithm\\imgs\\start.jpeg");
+            startBmp = (Bitmap)startImg;
+            startBmp = new Bitmap(startBmp, 374, 388);
+            if (gStart == null)
+            {
+                gStart = e.Graphics;
+            }
+
+            e.Graphics.DrawImage(startImg, 0, 0, 374, 388);
+
+            foreach (Line l in lines1)
+            {
+                e.Graphics.DrawLine(Pens.Red, l.p1, l.p2);
+            }
+        }
+
+        private void PanelFinish_Paint(object sender, PaintEventArgs e)
+        {
+            finishImg = Image.FromFile("C:\\Users\\Yvens\\Documents\\GitHub\\DisciplinaCG\\MorphingAlgorithm\\MorphingAlgorithm\\imgs\\finish.jpeg");
+            finishBmp = (Bitmap)finishImg;
+            finishBmp = new Bitmap(finishBmp, 374, 388);
+            if (gFinish == null)
+            {
+                gFinish = e.Graphics;
+            }
+
+            e.Graphics.DrawImage(finishImg, 0, 0, 374, 388);
+
+            foreach (Line l in lines2)
+            {
+                e.Graphics.DrawLine(Pens.Blue, l.p1, l.p2);
+            }
+        }
+
+        private void PanelStep_Paint(object sender, PaintEventArgs e)
+        {
+            if (gStep == null)
+            {
+                gStep = e.Graphics;
+            }
+
+            Bitmap bmp = new Bitmap(panelStep.Width, panelStep.Height);
+            Rectangle rec = new Rectangle(0, 0, 374, 388);
+            if (startBmp != null)
+            {
+                for (int i = 0; i < panelStep.Width; i++)
+                {
+                    for (int j = 0; j < panelStep.Height; j++)
+                    {
+                        Color p1 = startBmp.GetPixel(i, j);
+                        Color p2 = finishBmp.GetPixel(i, j);
+
+                        int r = Math.Abs(p1.R - p2.R);
+                        int b = Math.Abs(p1.B - p2.B);
+                        int g = Math.Abs(p1.G - p2.G);
+
+                        bmp.SetPixel(i, j, Color.FromArgb(r, g, b));
+                    }
+                }
+            }
+            
+            e.Graphics.DrawImage(bmp, rec);
+        }
+
+        private void PanelStart_Click(object sender, EventArgs e)
+        {
+            Panel panel = (Panel)sender;
+            Point point = panel.PointToClient(Cursor.Position);
+
+            if (!line1)
+            {
+                p1 = point;
+                line1 = true;
+            }
+            else
+            {
+                lines1.Add(new Line(point, p1));
+                line1 = false;
+
+                labelStart.Text = "Qtde: " + lines1.Count + " Adicionado: " + point + " e " + p1;
+
+                panel.Invalidate();
+            }
+        }
+
+        private void PanelFinish_Click(object sender, EventArgs e)
+        {
+            Panel panel = (Panel)sender;
+            Point point = panel.PointToClient(Cursor.Position);
+
+            if (!line2)
+            {
+                p2 = point;
+                line2 = true;
+            }
+            else
+            {
+                lines2.Add(new Line(point, p2));
+                line2 = false;
+
+                labelFinish.Text = "Qtde: " + lines2.Count + " Adicionado: " + point + " e " + p2;
+
+                panel.Invalidate();
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (lines1.Count > 0)
+            {
+                lines1.RemoveAt(lines1.Count - 1);
+            }
+
+            panelStart.Invalidate();
+        }
+
+        private void buttonFinish_Click(object sender, EventArgs e)
+        {
+            if (lines2.Count > 0)
+            {
+                lines2.RemoveAt(lines2.Count - 1);
+            }
+
+            panelFinish.Invalidate();
+        }
+
+        private void buttonStep_Click(object sender, EventArgs e)
+        {
+            panelStep.Invalidate();
+            //paintStep();
+        }
+
+        private void paintStep()
+        {
+            Graphics gr = panelStep.CreateGraphics();
+
+            Bitmap bmp = new Bitmap(374, 388);
+            if (startBmp != null)
+            {
+                for (int i = 0; i < panelStep.Width; i++)
+                {
+                    for (int j = 0; j < panelStep.Height; j++)
+                    {
+                        Color p1 = startBmp.GetPixel(i, j);
+                        Color p2 = finishBmp.GetPixel(i, j);
+
+                        int r = (p1.R + p2.R) / 2;
+                        int b = (p1.B + p2.B) / 2;
+                        int g = (p1.G + p2.G) / 2;
+
+                        bmp.SetPixel(i, j, Color.FromArgb(r, g, b));
+                    }
+                }
+            }
+
+            gr.DrawImage(bmp, 0, 0, 374, 388);
+
+            panelStep.Invalidate();
+        }
+    }
+
+    class Line
+    {
+        public Point p1;
+        public Point p2;
+
+        public Line(Point p1, Point p2)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+    }
+}
